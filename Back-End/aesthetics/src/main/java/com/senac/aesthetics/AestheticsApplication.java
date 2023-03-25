be832b58eeb838a07b41c820028579d9dfa4fea7
leonardooliveira <leonardovo.iel@fieg.com.br>
2023-03-24 21:49:12 -0300

    Incluir a 1ª Versão do Back-End

---
+++ Back-End/aesthetics/src/main/java/com/senac/aesthetics/AestheticsApplication.java
@@ -0,0 +1,13 @@
+package com.senac.aesthetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestheticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AestheticsApplication.class, args);
+	}
+
+}
---
+++ Back-End/aesthetics/src/test/java/com/senac/aesthetics/AestheticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.aesthetics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AestheticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
