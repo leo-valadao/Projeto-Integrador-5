1ca6b6c3fdc1303a20de9b6431092179162ef636
leonardooliveira <leonardovo.iel@fieg.com.br>
2023-04-02 20:13:27 -0300

    Inserir Versão Inicial da Tabela dos Clientes (Não Terminado)

--- Front-End Leonardo/Aesthetics/src/app/clientes/tabela-clientes/tabela-clientes.component.ts
+++ Front-End Leonardo/Aesthetics/src/app/clientes/tabela-clientes/tabela-clientes.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabela-clientes',
   templateUrl: './tabela-clientes.component.html',
   styleUrls: ['./tabela-clientes.component.less']
 })
-export class TabelaClientesComponent {
+export class TabelaClientesComponent implements OnInit {
+
+  // Variáveis:
+  
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
