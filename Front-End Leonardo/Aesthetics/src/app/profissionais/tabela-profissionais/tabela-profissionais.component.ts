6f7c2f3bbf3bb8e75863ce718e52448fdf0831a8
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-04-09 15:26:27 -0300

    Incluir a 1ª Versão da Tabela dos Profissionais

---
+++ Front-End Leonardo/Aesthetics/src/app/profissionais/tabela-profissionais/tabela-profissionais.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-profissionais',
+  templateUrl: './tabela-profissionais.component.html'
+})
+export class TabelaProfissionaisComponent {
+
+}
