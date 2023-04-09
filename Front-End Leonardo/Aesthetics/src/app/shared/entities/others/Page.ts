ea63a15b7a3b7a89c41267696675f03b5babb374
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-04-09 11:11:49 -0300

    Inserir 1 Nova Entidade de Paginação Para as Tabelas do Projeto

---
+++ Front-End Leonardo/Aesthetics/src/app/shared/entities/others/Page.ts
@@ -0,0 +1,31 @@
+export class Page<T> {
+
+    public content!: T[];
+    pageable!: {
+        sort: {
+            empty: boolean
+            sorted: boolean
+            unsorted: boolean
+        }
+        offset: number
+        pageSize: number
+        pageNumber: number
+        paged: boolean
+        unpaged: boolean
+    }
+    totalPages!: number
+    totalElements!: number
+    last!: boolean
+    size!: number
+    number!: number
+    sort!: {
+        empty: boolean
+        sorted: boolean
+        unsorted: boolean
+    }
+    numberOfElements!: number
+    first!: boolean
+    empty!: boolean
+
+    constructor() { }
+}
